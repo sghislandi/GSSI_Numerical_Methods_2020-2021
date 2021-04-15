c8cf51a4b3f4a92e6c231ea2ff5ef95bc6cab643
Cecilia Ferrari <cecilia.ferrari@gssi.it>
2021-04-15 14:24:00 +0200

    new folder organization

---
+++ pyplots/simpson_integration/error_plotter.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N, err = np.loadtxt('../../build/output/simpson_integration/simpson_approximation_errors.txt', skiprows = 1, unpack = True)
+
+plt.plot(N,np.abs(err),'o')
+plt.title('Simpson rule integral approximation error')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Number of divisions')
+plt.ylabel('Error')
+
+plt.savefig('simpson_approximation_errors.pdf')
+print("Output saved at ../build/output/simpson_integration/simpson_approximation_errors.pdf")
