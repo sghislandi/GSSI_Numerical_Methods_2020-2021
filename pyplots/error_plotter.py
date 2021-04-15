2058adbaadd6095514cdeaea51cd355cf9cded8a
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-04-15 10:19:40 +0200

    Create output directory at the compiling step

--- pyplots/error_plotter.py
+++ pyplots/error_plotter.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N, err = np.loadtxt('../output/simpson_integration/simpson_approximation_errors.txt', skiprows = 1, unpack = True)
+N, err = np.loadtxt('../build/output/simpson_integration/simpson_approximation_errors.txt', skiprows = 1, unpack = True)
 
 plt.plot(N,np.abs(err),'o')
 plt.title('Simpson rule integral approximation error')
@@ -10,4 +10,5 @@ plt.yscale('log')
 plt.xlabel('Number of divisions')
 plt.ylabel('Error')
 
-plt.savefig('../output/simpson_integration/simpson_approximation_errors.pdf')
+plt.savefig('../build/output/simpson_integration/simpson_approximation_errors.pdf')
+print("../build/output/simpson_integration/simpson_approximation_errors.pdf plot saved")
