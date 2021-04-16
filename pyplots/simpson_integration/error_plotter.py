4054f8408203d54b84eeab5a8953804b16ef58c0
Cecilia Ferrari <cecilia.ferrari@gssi.it>
2021-04-16 16:16:44 +0200

    improved style in pyplotter

--- pyplots/simpson_integration/error_plotter.py
+++ pyplots/simpson_integration/error_plotter.py
@@ -10,15 +10,10 @@ except:
     print("No output file found")
     exit()
 
-<<<<<<< HEAD
+
 plt.plot(2**N,np.abs(err),'o')
 plt.title('Simpson rule integral approximation error')
 plt.xscale('log',base=2)
-=======
-plt.plot(N,np.abs(err),'o')
-plt.title('Simpson method approximation error')
-plt.xscale('log')
->>>>>>> 4ddecfb5dea0b20fc262630c2acec7ebe1a61f07
 plt.yscale('log')
 plt.xlabel('Number of divisions')
 plt.ylabel(r'$\left|I_{Simpson} - I\right|$')
