98d223cb746542a9fc0052b96d7ffa2bb976c2dc
Stefano Ghislandi <stefano.ghislandi@gssi.it>
2021-05-06 15:42:09 +0200

    CN Loop

--- src/PDE/PDE.cpp
+++ src/PDE/PDE.cpp
@@ -16,6 +16,53 @@ double ComputeRMS(std::vector<double> u, const double xMin, const double dx){
     return std::sqrt(RMS / (double) N);
 }
 
+
+std::vector<double> CNLoop(int N, const double dt, const double tMax){
+    std::vector<double> errors;
+    
+    const int xDivision = N - 1;
+    const double xMin = -1.;
+    const double xMax = 1.;
+    const double dx = (double)(xMax-xMin) / xDivision;
+
+    const double D = 0.1;
+    double alpha = D*dt/2./dx/dx;
+
+    std::vector<double> f(N,0);
+    std::vector<double> Q(N-2,0);
+
+    for(int i = 1; i < N-1 ; i++){
+        Q[i-1] = M_PI*M_PI/4.*D*dt*uAnalytical(xMin + (double)i*dx);
+    }
+
+    //Intermediate functions 
+    std::vector<double> f0 = f;    
+    std::vector<double> f1(N,0);
+    std::vector<double> f2(N,0);
+    std::vector<double> f4(N,0);
+    std::vector<double> f8(N,0);
+    std::vector<double> f16(N,0);
+
+    double t = 0.;
+    for(int tIndex = 0; tIndex<tMax/dt; tIndex ++){
+        f = CrankNicolson(f, Q, alpha);
+        errors.push_back(ComputeRMS(f, xMin, dx));
+        if(N == 256){
+            if(tIndex == 1000) f1 = f;
+            if(tIndex == 2000) f2 = f;
+            if(tIndex == 4000) f4 = f;
+            if(tIndex == 8000) f8 = f;
+            if(tIndex == 16000) f16 = f;
+        }
+    }
+    if(N == 256){
+        saveIntermediateFunctions(xMin,dx,N,f0,f1,f2,f4,f8,f16,f);
+    }
+
+    return errors;
+}
+
+
 int main(){
     std::ofstream output("output/PDE/PDE_2Norm.txt", std::ofstream::trunc);
 
